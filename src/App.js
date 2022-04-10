46b3d5b47b1b7a854dda7db3c4c2e4ea5047c0f9
Ian Murray [I] <imurray101coding@gmail.com>
2022-04-09 17:22:28 -0700

    starter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Coaching from './pages/coaching';
+import Resume from './pages/resume';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact component={About}/>
+        <Route path='/coaching' exact component={Coaching}/>
+        <Route path='/resume' exact component={Resume}/>
+        <Route path='/contact' exact component={Contact}/>
+
+      </Switch>
+    </Router>
   );
 }
 
