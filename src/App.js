cbdd18550a948f72d6e2e1f2abe352962d065013
Ian Murray [I] <imurray101coding@gmail.com>
2022-04-09 17:35:28 -0700

    gitignore for react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './pages/about';
 import Contact from './pages/contact';
@@ -12,13 +12,13 @@ function App() {
   return (
     <Router>
       <Navbar/>
-      <Switch>
+      <Routes>
         <Route path='/' exact component={About}/>
         <Route path='/coaching' exact component={Coaching}/>
         <Route path='/resume' exact component={Resume}/>
         <Route path='/contact' exact component={Contact}/>
 
-      </Switch>
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/coaching' activeStyle>
+            Coaching
+          </NavLink>
+          <NavLink to='/resume' activeStyle>
+            Resume
+          </NavLink>
+          <NavLink to='/contact' activeStyle>
+            Contact
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
